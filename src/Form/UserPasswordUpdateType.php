9129fc254c23785f37c227b22b4aabb769e505fb
Michael Rodin <mikhail.rodin@satellite-soft.ru>
2021-06-12 15:39:35 +0400

    Fixed routing bug && Replaced arrow characters to HTML charcode

--- src/Form/UserPasswordUpdateType.php
+++ src/Form/UserPasswordUpdateType.php
@@ -6,7 +6,6 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
